78e73917549f38021374ea80d1c7f935921f8bc6
traipe <jimenatraipe@gmail.com>
2024-03-11 20:11:15 -0300

    se modifica el codigo de js para su mejor funcionamiento, se arreglan errores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
+$(document).ready(function() {
+  // Inicializar el carrusel al cargar la página
+  $('#carouselExampleSlidesOnly').carousel();
 
+  // Evento click en botón "Enviar"
+  $("#enviarMensaje").click(function(e) {
+    e.preventDefault(); // Evitar que el formulario se envíe
+
+    // Mostrar el mensaje de éxito
+    alert("Mensaje enviado con éxito");
+  });
+});
 
 
 
