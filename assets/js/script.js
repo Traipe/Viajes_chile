714184943c54fefe3b1bd16c6fd820114a9ff641
traipe <jimenatraipe@gmail.com>
2024-03-11 10:38:53 -0300

    subiendo el trabajo viajes_chile

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+ 
+  // Activar Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+});
+
+//Esconder h3
+$("h3").clickhide();
+
+
+//Carrusel Slide
+
+
+  // Evento click en botón "Enviar"
+  $("#enviarMensaje").click(function(){
+    alert(Mensaje);
+   
+  });
+
+  
+
+
+
+
+
+
+//fin  
+});
