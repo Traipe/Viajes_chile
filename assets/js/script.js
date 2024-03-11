d07df6027166c027938009a41e881d72be1d8828
traipe <jimenatraipe@gmail.com>
2024-03-11 20:09:42 -0300

    se cambia el color del texto del boton enviar a white, se añade una class para mensajeEnviado y se añade en js el codigo de funcionamiento. se elimina dos jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,3 @@
-$(document).ready(function(){
-  $('#carouselExampleSlidesOnly').carousel();
-});
- 
-
-
-//Esconder h3
-$("h3").clickhide();
-
-
-//Carrusel Slide
-
-
-  // Evento click en botón "Enviar"
-  $("#enviarMensaje").click(function(){
-    alert(Mensaje);
-   
-  });
-
-  
-
 
 
 
